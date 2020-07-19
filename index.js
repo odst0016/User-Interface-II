5fa22de67baa9a58870b6726b27ab653355a8287
Nathan Patterson <odst0016@gmail.com>
2020-07-18 21:13:28 -0400

    refactoring css and html

--- index.js
+++ index.js
@@ -1,105 +1,36 @@
-//Project 1 Model
-var project1 = document.getElementById("project1Modal");
-var btn = document.getElementById("myBtn");
-var project1span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  project1.style.display = "block";
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+// gets input from open buttons
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+// gets input from clicking outside of modal
+overlay.addEventListener('click', () => {
+    const Modals = document.querySelectorAll('.modal.active')
+    Modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+// gets input from close button
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+// opens the modal
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
 }
-project1span.onclick = function() {
-  project1.style.display = "none";
+// closes the modal
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
 }
-//Project 2 Model
-var project2 = document.getElementById("project2Modal");
-var btn2 = document.getElementById("myBtn2");
-var project2span = document.getElementsByClassName("close2")[0];
-btn2.onclick = function() {
-  project2.style.display = "block";
-}
-project2span.onclick = function() {
-  project2.style.display = "none";
-}
-//Project 3 Model
-var project3 = document.getElementById("project3Modal");
-var btn3 = document.getElementById("myBtn3");
-var project3span = document.getElementsByClassName("close3")[0];
-btn3.onclick = function() {
-  project3.style.display = "block";
-}
-project3span.onclick = function() {
-  project3.style.display = "none";
-}
-//Project 4 Model
-var project4 = document.getElementById("project4Modal");
-var btn4 = document.getElementById("myBtn4");
-var project4span = document.getElementsByClassName("close4")[0];
-btn4.onclick = function() {
-  project4.style.display = "block";
-}
-project4span.onclick = function() {
-  project4.style.display = "none";
-}
-//Project 5 Model
-var project5 = document.getElementById("project5Modal");
-var btn5 = document.getElementById("myBtn5");
-var project5span = document.getElementsByClassName("close5")[0];
-btn5.onclick = function() {
-  project5.style.display = "block";
-}
-project5span.onclick = function() {
-  project5.style.display = "none";
-}
-//Project 6 Model
-var project6 = document.getElementById("project6Modal");
-var btn6 = document.getElementById("myBtn6");
-var project6span = document.getElementsByClassName("close6")[0];
-btn6.onclick = function() {
-  project6.style.display = "block";
-}
-project6span.onclick = function() {
-  project6.style.display = "none";
-}
-//Project 7 Model
-var project7 = document.getElementById("project7Modal");
-var btn7 = document.getElementById("myBtn7");
-var project7span = document.getElementsByClassName("close7")[0];
-btn7.onclick = function() {
-  project7.style.display = "block";
-}
-project7span.onclick = function() {
-  project7.style.display = "none";
-}
-//Project 8 Model
-var project8 = document.getElementById("project8Modal");
-var btn8 = document.getElementById("myBtn8");
-var project8span = document.getElementsByClassName("close8")[0];
-btn8.onclick = function() {
-  project8.style.display = "block";
-}
-project8span.onclick = function() {
-  project8.style.display = "none";
-}
-//Resume Model
-var projectR = document.getElementById("projectRModal");
-var btnR = document.getElementById("myBtnR");
-var projectRspan = document.getElementsByClassName("closeR")[0];
-btnR.onclick = function() {
-  projectR.style.display = "block";
-}
-projectRspan.onclick = function() {
-  projectR.style.display = "none";
-}
-//Contact Model
-var projectC = document.getElementById("projectCModal");
-var btnC = document.getElementById("myBtnC");
-var projectCspan = document.getElementsByClassName("closeC")[0];
-btnC.onclick = function() {
-  projectC.style.display = "block";
-}
-projectCspan.onclick = function() {
-  projectC.style.display = "none";
-}
-
-
-
-
-
